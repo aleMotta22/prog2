84f0684db9b04978344142c61bc46e09089d3727
AleLiberty <37674557+toMattri@users.noreply.github.com>
2020-01-29 21:01:05 +0100

    Add files via upload

--- PROG/C/merge.cpp
+++ PROG/C/merge.cpp
@@ -1,5 +1,8 @@
 #include<iostream>
 #include<fstream>
+#include <stdio.h>
+#include <string.h>
+
 using namespace std;
 
 
@@ -9,6 +12,15 @@ void CaricaVett(int* a, int n, ifstream& input){
 	}
 }
 
+
+void StampaVett(int* a, int n){
+	for(int i = 0; i < n; i++){
+		cout<<a[i]<<" ";
+	}
+	cout<<"\n";
+	
+}
+
 void StampaVett(int* a, int n, ofstream& output){
 	for(int i = 0; i < n; i++){
 		output<<a[i]<<" ";
@@ -16,27 +28,90 @@ void StampaVett(int* a, int n, ofstream& output){
 	
 }
 
-void merge(int v[], int low, int mid, int high) {
-	int b[50];
-	int h = low;
-	int i = low;
-	int j = mid+1;
+void merge(int *arr, int low, int high, int mid)
 
-	while( (h<=mid) && (j<=high) ) {
-		if(v[h] <= v[j]) b[i] = v[h++];
-		else b[i] = v[j++];
-  		i++;
-	}
- 	if(h>mid)
-  		for(int k=j; k<=high; k++) b[i++]=v[k];
+{
+
+    int i, j, k, c[5001];
+
+    i = low;
+
+    k = low;
+
+    j = mid + 1;
+
+    while (i <= mid && j <= high) {
+
+        if (arr[i] < arr[j]) {
+
+            c[k] = arr[i];
+
+            k++;
+
+            i++;
+
+        }
+
+        else  {
+
+            c[k] = arr[j];
+
+            k++;
+
+            j++;
+
+        }
+
+    }
+
+    while (i <= mid) {
+
+        c[k] = arr[i];
+
+        k++;
+
+        i++;
+
+    }
+
+    while (j <= high) {
+
+        c[k] = arr[j];
+
+        k++;
+
+        j++;
+
+    }
+    for (i = low; i < k; i++)  {
+        arr[i] = c[i];
+
+    }
+}
+
+
+void merge2(int *a, int start, int middle, int end){
+	
+    middle+=1;
+	int i = 0;
+	int app[501];
+	
+	while(start <= middle && middle <= end){
+ 		if( a[start] < app[middle] ) {app[i] = a[start]; start++;}
+ 	else{ app[i] = a[middle]; middle++;}
+ 		i++;
+ 	}
+ 	if(start<= middle)
+  		while (i <= end) {app[i] = a[start]; start++;}
  	else
-		for(int k=h; k<=mid; k++) b[i++]=v[k];
- 	for(int k=low; k<=high; k++) v[k]=b[k];
+		while(i<= end) {app[i] = a[middle]; middle++;} 
+		cout<<app[0]<<"\n";
+ 	memcpy(a,&app,sizeof(a)*end+1); 
  	
  	
 }
 
-void mergesort(int *v, int low, int high, int& cont) {
+void mergesort(int v[], int low, int high, int& cont) {
 
 	if( low<high ) {
 		cont+=v[low];
@@ -47,12 +122,17 @@ void mergesort(int *v, int low, int high, int& cont) {
  	}
 }
 
+
+
+
+
 void metodo(ifstream& input, ofstream& output){
 	int N;
 	input>>N;
 	int cont = 0;
 	int *vett = new int[N];
 	CaricaVett(vett, N, input);
+	StampaVett(vett,N);
 	mergesort(vett, 0, N-1, cont);
 	
 	output <<cont << endl;
